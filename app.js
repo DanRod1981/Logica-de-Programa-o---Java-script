fcca3024ad6828c966ee67a487e9d7da3be76630
DanRod1981 <166569506+DanRod1981@users.noreply.github.com>
2026-01-06 11:52:40 -0300

    Adicionando arquivos do projeto JavaScript
    código do jogo numero secreto

---
+++ app.js
@@ -0,0 +1,22 @@
+alert('Boas vindas ao jogo do numero secreto');
+let numeroSecreto = 8;
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s
+while (chute != numeroSecreto) {
+    chute = prompt ('escolha um número entre 1 e 10');
+//se chute for igual ao numero secreto
+    if (chute == numeroSecreto){
+        alert(`isso ai!!!!!! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+    } else{
+        if (chute > numeroSecreto) {
+            alert (`O numero secreto é menor que ${chute}`);
+        } else {
+            alert(`O numero secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++
+        }
+    }
