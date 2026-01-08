06652bc5da27b708cda32d4096373586a7870f34
DanRod1981 <166569506+DanRod1981@users.noreply.github.com>
2026-01-08 12:28:52 -0300

    Inserindo break para corrigir fluxo do jogo

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ while (chute != numeroSecreto) {
     chute = prompt ('escolha um número entre 1 e 10');
 //se chute for igual ao numero secreto
     if (chute == numeroSecreto){
-        alert(`isso ai!!!!!! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+        break;  
     } else{
         if (chute > numeroSecreto) {
             alert (`O numero secreto é menor que ${chute}`);
@@ -20,3 +20,9 @@ while (chute != numeroSecreto) {
         tentativas++
         }
     }
+    
+if (tentativas > 1) {
+    alert(`isso ai!!!!!! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+    alert(`isso ai!!!!!! voce descobriu o numero secreto ${numeroSecreto} com ${tentativas} tentativa`)
+}
